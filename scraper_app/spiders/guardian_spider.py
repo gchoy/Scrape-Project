eb4895fc6c8c5d9b914c1a97fb8e8b700ac47882
gchoy <gabriela.choy@gmail.com>
2016-05-14 03:51:12 -0700

    spider update

--- scraper_app/spiders/guardian_spider.py
+++ scraper_app/spiders/guardian_spider.py
@@ -15,7 +15,7 @@ class GuardianNews(scrapy.Spider):
         author = response.xpath(".//*[@id='article']/div[2]/div/div[1]/div[2]/p[1]/span/a/span/text()").extract()
         title = response.xpath(".//*[@id='article']/header/div[1]/div/div/h1/text()").extract()
         content = response.xpath(".//*[@id='article']/div[2]/div/div[1]/div[3]/p/text() |//*[@id='article']/div[2]/div/div[1]/div[3]/h2/text()").extract()
-        #url = article['url']
-        print date, author,title,content
+        url = response.xpath(".//link[18]/@href").extract()
+        print date, author,title,content,url
 
 
