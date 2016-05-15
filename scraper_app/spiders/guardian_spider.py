69a805ecc1c4e4ae480b398a5dbd1f2b04fee48d
gchoy <gabriela.choy@gmail.com>
2016-05-14 20:31:45 -0700

    spider update

--- scraper_app/spiders/guardian_spider.py
+++ scraper_app/spiders/guardian_spider.py
@@ -2,20 +2,26 @@ import scrapy
 
 
 
-class GuardianNews(scrapy.Spider):
+class DmozSpider(scrapy.Spider):
     name = "theguardian"
     allowed_domains = ["theguardian.com"]
     start_urls = [
-      "http://www.theguardian.com/environment/2016/may/11/canada-wildfire-environmental-impacts-fort-mcmurray"
+      "http://www.theguardian.com/environment/2016/may/11/canada-wildfire-environmental-impacts-fort-mcmurray",
+      "http://www.theguardian.com/environment/2016/may/14/scientists-people-power-find-disease-ash-resistant-trees"
     ]
 
     def parse(self, response):
         """Extracts data from the theguardian website"""
-        date = response.xpath(".//*[@id='article']/div[2]/div/div[1]/div[2]/p[2]/time[1]/text()").extract()
-        author = response.xpath(".//*[@id='article']/div[2]/div/div[1]/div[2]/p[1]/span/a/span/text()").extract()
-        title = response.xpath(".//*[@id='article']/header/div[1]/div/div/h1/text()").extract()
-        content = response.xpath(".//*[@id='article']/div[2]/div/div[1]/div[3]/p/text() |//*[@id='article']/div[2]/div/div[1]/div[3]/h2/text()").extract()
-        url = response.xpath(".//link[18]/@href").extract()
-        print date, author,title,content,url
 
+        for sel1 in response.xpath(".//*[@id='article']"):
+
+            date = sel1.xpath("////p[2]/time[1]/text()").extract()
+            author = sel1.xpath("////p[1]/span/a/span/text()").extract()
+            title = sel1.xpath("header/div[1]/div/div/h1/text()").extract()
+            content = sel1.xpath('////p/text() | ////h2/text()').extract()
+
+        for sel2 in response.xpath('//html'):
+
+            url = sel2.xpath(".//link[18]/@href").extract()
+        print date, author,title,content,url
 
