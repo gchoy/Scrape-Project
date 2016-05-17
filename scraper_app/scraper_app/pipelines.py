11f1c7bbbe7be0da70e3b7f97543c6cff98614b6
gchoy <gabriela.choy@gmail.com>
2016-05-17 15:10:07 -0700

    updating pipelines

--- scraper_app/scraper_app/pipelines.py
+++ scraper_app/scraper_app/pipelines.py
@@ -9,7 +9,7 @@ import pymongo
 
 from scrapy.conf import settings
 from scrapy.exceptions import DropItem
-#from scrapy import log
+
 
 class MongoDBPipeline(object):
 
@@ -22,14 +22,13 @@ class MongoDBPipeline(object):
         self.collection = db[settings['MONGODB_COLLECTION']]
 
     def process_item(self, item, spider):
-        #valid = True
-        #for data in item:
-        #    if not data:
-        #       valid = False
-        #       raise DropItem("Missing {0}!".format(data))
-        #if valid:
-        self.collection.insert(dict(item))
-            #log.msg("Article added to MongoDB database!",
-            #        level=log.DEBUG, spider=spider)
+        valid = True
+        for data in item:
+            if not data:
+               valid = False
+               raise DropItem("Missing {0}!".format(data))
+        if valid:
+           self.collection.insert(dict(item))
+            
 
         return item
